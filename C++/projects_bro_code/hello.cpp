a6be6568c106e022993909fd928af175b7e63234
OnetoTommy <xingguangc09@gmail.com>
2025-05-23 16:30:49 -0700

    Math

--- C++/projects_bro_code/hello.cpp
+++ C++/projects_bro_code/hello.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 
+
+// typedef std::string text_t;
+// typedef int num;
+// using text_t = std::string;
+// using num = int;
+
 int main(){
   // Section2
-  const double pi = 3.14;
-  double r = 10;
-  double circumference = 2 * r * pi;
-  std::cout << circumference << '\n';
+  // const double pi = 3.14;
+  // double r = 10;
+  // double circumference = 2 * r * pi;
+  // std::cout << circumference << '\n';
+
+  // text_t firstName = "Bro";
+  // num age = 20;
+  // std::cout << firstName << '\n';
+  // // std::cout << age << '\n';
+  // int students = 20;
+  // // students = students + 1;
+  // // students += 1; 
+  // // students ++;
+  // students %= 2;
+  // std::cout << (char) 100 << '\n';
+
+  // cin >> extraction operator
+  // cout >> insertion operator
+  // std::string name;
+  // std::cout << "What's your name?";
+  // // std::cin >> name;
+  // std:getline(std::cin >> std::ws, name); //std::ws is used when it is not the first cin.
+  // std::cout << "Hello " << name <<'\n';
+
+
+  // max math
+  double x = 1;
+  double y = 2;
+  // double z = std::max(x, y); // std::min(x, y)
+  // double l = std::pow(2, 4); // 2^4
+  // double m = std::sqrt(9);
+  // double n = std::abs(-1);
+  //double z = round(3.99); // round 
+   double z = ceil(3.1); //round up
+  // double z = floor(3.99); //round down
+  std::cout << z << '\n';
+
+
+  
+
 
 
 
