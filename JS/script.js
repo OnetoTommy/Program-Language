bd910c6b9ad07b6705f1895428b0c70d6db47192
OnetoTommy <xingguangc09@gmail.com>
2025-06-02 14:42:03 -0700

    JS
    console.log
    document.getElementId().textContent
    `${}`

---
+++ JS/script.js
@@ -0,0 +1,37 @@
+// Variable
+let fullName = "Bro Code";
+let age = 25;
+let student = false;
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = student;
+
+// let x = 20;
+// let y = "Hello";
+// let m = 'A';
+// const z = 30;
+// x = 70
+// console.log(x);
+// console.log(typeof y)
+// console.log(typeof m);
+
+// console.log(`You are ${z} years old.`);
+
+// let online = true;
+// let offline = false;
+// console.log(`Bro is online: ${online}`);
+
+
+// console.log(`Hello`);
+// console.log(`I like pizza!`);
+
+// window.alert(`This is alert`)
+
+//This is a comment
+/*
+  This 
+  is
+*/
+// document.getElementById("myH1").textContent = "Hello Name";
+// document.getElementById("myP").textContent = "I like pizza!"
+
