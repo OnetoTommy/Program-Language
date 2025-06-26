09481860e2baf14994fc3d9a331bc1e92dc79d1c
OnetoTommy <xingguangc09@gmail.com>
2025-06-26 15:20:29 -0700

    StopWatch

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,72 @@
 /*
+StopWatch
+*/
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapseTime = 0;
+let isRunning = false;
+
+function start(){
+  if(!isRunning){
+    startTime = Date.now() - elapseTime;
+    timer = setInterval(update, 10);
+    isRunning = true;
+  }
+
+}
+function stop(){
+  if(isRunning){
+    clearInterval(timer)
+    elapseTime = Date.now() - startTime;
+    isRunning = false;
+  }
+}
+
+function reset(){
+  clearInterval(timer)
+  startTime = 0;
+  elapseTime = 0;
+  isRunning = false;
+  display.textContent = "00:00:00:00"
+
+}
+function update(){
+  const currentTime = Date.now();
+  elapseTime = currentTime - startTime;
+
+  let hours = Math.floor(elapseTime / (1000 * 60 * 60));
+  let minutes = Math.floor(elapseTime / (1000 * 60) % 60);
+  let seconds = Math.floor(elapseTime / (1000) % 60);
+  let milliseconds = Math.floor(elapseTime % 1000 / 10);
+  hours = String(hours).padStart(2, "0");
+  minutes = String(minutes).padStart(2, "0");
+  seconds = String(seconds).padStart(2, "0");
+  milliseconds = String(milliseconds).padStart(2, "0");
+
+  display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+
+}
+
+
+
+
+/*
 setTimeout = allows you to schedule the execution of a function after an amount of time
 */
-
-function updateClock(){
-  const now = new Date();
-  let hours = now.getHours();
-  const meridiem = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12;
-  hours = hours.toString().padStart(2, 0);
-  const minutes = now.getMinutes().toString().padStart(2, 0);
-  const seconds = now.getSeconds().toString().padStart(2, 0);
-  const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
-  document.getElementById("clock").textContent = timeString;
-}
-updateClock()
-setInterval(updateClock, 1000);
+// function updateClock(){
+//   const now = new Date();
+//   let hours = now.getHours();
+//   const meridiem = hours >= 12 ? "PM" : "AM";
+//   hours = hours % 12 || 12;
+//   hours = hours.toString().padStart(2, 0);
+//   const minutes = now.getMinutes().toString().padStart(2, 0);
+//   const seconds = now.getSeconds().toString().padStart(2, 0);
+//   const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+//   document.getElementById("clock").textContent = timeString;
+// }
+// updateClock()
+// setInterval(updateClock, 1000);
 
 
 
